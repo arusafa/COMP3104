6606118862a7be5d995ea64125302c7cb0af1bf1
safaarugbc <safa.aru@georgebrown.ca>
2022-10-03 20:48:18 -0400

    adding all changes at once which includes package.json work and app src

---
+++ build/precache-manifest.30f09ab6aef5287324d9a1613889e04e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00073c536394dc2779ec",
+    "url": "/arusafa/COMP3104/static/css/main.c7c540fc.chunk.css"
+  },
+  {
+    "revision": "00073c536394dc2779ec",
+    "url": "/arusafa/COMP3104/static/js/main.00073c53.chunk.js"
+  },
+  {
+    "revision": "2ed6ae64231d8cb7479e",
+    "url": "/arusafa/COMP3104/static/js/runtime~main.2ed6ae64.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/arusafa/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "021cff0442085964bb6f4fad3d62bebf",
+    "url": "/arusafa/COMP3104/index.html"
+  }
+];
